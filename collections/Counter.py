fac278b1550de5ca0eda96b76953854dcdf5cb97
Ahmed Yamany <yamany28@students.mans.edu.eg>
2020-09-09 14:58:24 +0200

    delete some bugs

--- collections/Counter.py
+++ collections/Counter.py
@@ -1,18 +1,4 @@
-#
-# X = int(input())
-# shoe_size = list(map(int, input().split()))
-# N = int(input())
-# A = []
-# for i in range(N):
-#     x = list(map(int, input().split()))
-#     if x[0] in shoe_size:
-#         A.append(x[1])
-#
-#
-# A = list(set(A))
-#
-# print(sum(A))
-from collections import Counter
+
 X = int(input())
 shoe_size = list(map(int, input().split()))
 
