6aa215dc968979d07c512c36aa4782c1f97f5442
Ahmed Yamany <yamany28@students.mans.edu.eg>
2020-09-11 16:46:23 +0200

    orderedDict onother solve

--- collections/orderedDict.py
+++ collections/orderedDict.py
@@ -1,13 +1,1 @@
-from collections import OrderedDict
-N = int(input())
-dct = OrderedDict()
-for i in range(N):
-    name_price = input().split()
-    string = ' '.join(name_price[:-1])
-
-    if string not in dct:
-        dct[string] = int(name_price[-1])
-    else:
-        dct[string] += int(name_price[-1])
-for i, j in dct.items():
-    print(i, j)
+from collections import OrderedDictfrom collections import defaultdictN = int(input())dct = OrderedDict()d = defaultdict(list)for i in range(N):    name_price = input().split()    string = ' '.join(name_price[:-1])    d[string].append(int(name_price[-1]))for i, j in d.items():    print(i, sum(j))#     if string not in dct:#         dct[string] = int(name_price[-1])#     else:#         dct[string] += int(name_price[-1])# for i, j in dct.items():#     print(i, j)
