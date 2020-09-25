8b827d5f67e1d01723d67b0cef3bb4fab0abeae9
Ahmed Yamany <yamany28@students.mans.edu.eg>
2020-09-25 14:01:30 +0200

    Athlete Sort

---
+++ Build-ins/athleteSort.py
@@ -0,0 +1,11 @@
+n, m = list(map(int, input().split()))
+
+arr = []
+
+for _ in range(n):
+    arr.append(list(map(int, input().rstrip().split())))
+
+k = int(input())
+arr.sort(key=lambda x: x[k])
+for i in arr:
+    print(*i, sep=' ')
