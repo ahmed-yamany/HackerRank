3f87cbf7e37fabc9786279e075ab4fdc564cc322
Ahmed Yamany <yamany28@students.mans.edu.eg>
2020-10-21 19:04:46 +0200

    Decorator2

---
+++ Closures and Decorators/Decorators 2 - Name Directory.py
@@ -0,0 +1,17 @@
+def person_lister(f):
+    def inner(people):
+        people = sorted(people, key=lambda x: int(x[2]))
+        for i in people:
+            yield f(i)
+
+    return inner
+
+
+@person_lister
+def name_format(person):
+    return ("Mr. " if person[3] == "M" else "Ms. ") + person[0] + " " + person[1]
+
+
+if __name__ == '__main__':
+    people = [input().split() for i in range(int(input()))]
+    print(*name_format(people), sep='\n')
